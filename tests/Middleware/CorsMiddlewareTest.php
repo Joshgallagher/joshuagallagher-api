2d8fc6abc093da62050ad08dba3098acf2f66341
Joshua Gallagher <joshuagallagher@Joshuas-MacBook-Pro.local>
2017-10-29 16:24:44 +0000

    Implemented first cors test and created middleware logic.

--- tests/Middleware/CorsMiddlewareTest.php
+++ tests/Middleware/CorsMiddlewareTest.php
@@ -3,20 +3,36 @@
 namespace Tests\Middleware;
 
 use Tests\TestCase;
+use Illuminate\Http\Request;
+use App\Http\Middleware\Cors;
+use Illuminate\Http\Response;
 
 class CorsMiddlewareTest extends TestCase
 {
     /**
      * @test
      */
-    public function preflight_request_is_handled()
+    public function correct_headers_are_set()
     {
+        $request = Request::create('/api/articles?page=1', 'GET');
+
+        $middleware = new Cors;
+
+        $response = new Response;
+
+        $response = $middleware->handle($request, function () use ($response) {
+            return $response;
+        });
+
+        $this->assertEquals($response->headers->get('Access-Control-Allow-Origin'), '*');
+        $this->assertEquals($response->headers->get('Access-Control-Allow-Methods'), 'HEAD, GET, OPTIONS');
+        $this->assertEquals($response->headers->get('Access-Control-Allow-Headers'), 'Content-Type');
     }
 
     /**
      * @test
      */
-    public function correct_headers_are_set()
+    public function preflight_request_is_handled()
     {
     }
 }
