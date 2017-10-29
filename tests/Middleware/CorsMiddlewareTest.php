1ae1c9009ead048e5c816340256346a6b9a95720
Joshua Gallagher <joshuagallagher@Joshuas-MacBook-Pro.local>
2017-10-29 15:36:09 +0000

    Set up cors middleware test.

---
+++ tests/Middleware/CorsMiddlewareTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Middleware;
+
+use Tests\TestCase;
+
+class CorsMiddlewareTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function preflight_request_is_handled()
+    {
+    }
+
+    /**
+     * @test
+     */
+    public function correct_headers_are_set()
+    {
+    }
+}
